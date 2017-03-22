0da20e01c00021b2f012039ca57d0e008bc1bd6d
aula7 <pc716@gmail.com>
2017-03-22 13:39:41 -0500

    se Edito menu

--- app/src/main/java/com/jonmid/firstpartial/ProfileActivity.java
+++ app/src/main/java/com/jonmid/firstpartial/ProfileActivity.java
@@ -3,6 +3,9 @@ package com.jonmid.firstpartial;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -18,5 +21,16 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_contact, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
